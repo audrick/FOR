4d256d28aa3fe73e0684e3638c248044368ba0ce
Audric Kim <audrickim@gmail.com>
2012-12-10 17:27:44 -0500

    fixed registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
@@ -15,12 +16,34 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def login
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+    redirect_to @user
+  end
+
 
   def buyer_pref
     # @user = User.find(params[:id])
